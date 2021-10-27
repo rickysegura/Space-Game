bbe478c5eb76c7008392b96675af615c815fb087
Ricky Segura <88262093+rickysegura@users.noreply.github.com>
2021-10-27 12:28:41 -0600

    Main Python File

---
+++ game.py
@@ -0,0 +1,34 @@
+import pygame
+import os
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Ever Game")
+
+COLOR = (225, 205, 255)
+FPS = 60
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
+
+def draw_window():
+    WIN.fill(COLOR)
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    pygame.display.update()
+
+def main():
+  clock = pygame.time.Clock()
+  run = True
+  while run:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+    
+    draw_window()
+  
+  pygame.QUIT()
+
+if __name__ == "__main__":
+  main()
